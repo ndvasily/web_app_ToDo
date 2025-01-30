448f9a91d33c5cbec45e295e649842d0f3cacfb9
nenad <ndvasily@outlook.com>
2025-01-30 15:24:30 +0100

    Todos.txt modified with capital T.

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-def get_todos(filepath='todos.txt'):   # kreirame funkcija koja kje go cita fileot so Parametar.
+def get_todos(filepath='Todos.txt'):   # kreirame funkcija koja kje go cita fileot so Parametar.
     """ Read the saved Todo List and assign it to the variable todos.  """  #Doc String
     with open(filepath , 'r') as file:
         todos = file.readlines()    # mnogu poprofi e vaka
     return todos     # koj rezultat da go vrati taa funckija
 
 
-def write_todos(todos_arg, filepath='todos.txt' ):
+def write_todos(todos_arg, filepath='Todos.txt' ):
     """ Saves the new Todo list. """  #Doc String
     with open(filepath, 'w') as file:  # zapishuvanje na nov file so skraten metod (WITH)
         file.writelines(todos_arg)
